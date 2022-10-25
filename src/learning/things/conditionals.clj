b19d6a345070fdbf090ad8128bc6cbc50266d107
Ygor Simões <contato.ygorsimoes@gmail.com>
2022-10-25 20:26:22 -0300

    Create conditionals.clj

---
+++ src/learning/things/conditionals.clj
@@ -0,0 +1,30 @@
+(ns learning.things.conditionals)
+
+;; Basic conditional
+(let [number-one 2
+      numbe-two 12]
+  (println "Bigger or smaller:" (< number-one numbe-two)))
+
+(let [number-one 5
+      number-two 5
+      is-bigger? (> number-one number-two)]
+  (println "This is bigger?" is-bigger?))
+
+
+;; Conditional with if and else
+(def number-one 2)
+(def number-two 5)
+(def number-three 5)
+
+(defn sum [a, b]
+  (+ a b))
+
+(defn this-is-bigger?
+  [a, b, c]
+  (if (== c (sum a b))
+    ;; conditional case true
+    "Is bigger!")
+  ;; conditional case false
+  "Is not bigger!")
+
+(println (this-is-bigger? number-one, number-two, number-three))
