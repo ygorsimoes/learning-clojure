dfebec460d0e3616d6b7d317bfad12a57f78177e
Ygor Simões <contato.ygorsimoes@gmail.com>
2022-10-26 21:40:33 -0300

    update conditionals.clj

--- src/learning/basic/conditionals.clj
+++ src/learning/basic/conditionals.clj
@@ -1,6 +1,6 @@
 (ns learning.basic.conditionals)
 
-;; Basic conditional
+;; Basic conditional:
 (let [number-one 2
       numbe-two 12]
   (println "Bigger or smaller:" (< number-one numbe-two)))
@@ -11,20 +11,33 @@
   (println "This is bigger?" is-bigger?))
 
 
-;; Conditional with if and else
+;; Conditional with if and else:
 (def number-one 2)
 (def number-two 5)
-(def number-three 5)
+(def number-three 7)
 
 (defn sum [a, b]
   (+ a b))
 
-(defn this-is-bigger?
+;; Predicates:
+;; If version
+(defn if-is-bigger?
   [a, b, c]
-  (if (== c (sum a b))
-    ;; Conditional case true
-    "Is bigger!")
-  ;; Conditional case false
-  "Is not bigger!")
+  (if (> c (sum a b))
+    true
+    false))
 
-(println (this-is-bigger? number-one, number-two, number-three))
+;; When version
+(defn when-is-bigger?
+  [a, b, c]
+  (when (> c (sum a b))
+    true))
+
+;; Concise version
+(defn concise-is-bigger?
+  [a, b, c]
+  (> c (sum a b)))
+
+(println "\nVersion: if. Is bigger?" (if-is-bigger? number-one, number-two, number-three))
+(println "Version: when. Is bigger?" (when-is-bigger? number-one, number-two, number-three))
+(println "Version: concise. Is bigger?" (concise-is-bigger? number-one, number-two, number-three))
