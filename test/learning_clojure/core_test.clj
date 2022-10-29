383ccb2f1b08c16a0fbf4c67632ec23c148206b5
Ygor Simões <contato.ygorsimoes@gmail.com>
2022-10-28 21:52:24 -0300

    updated project name

--- test/learning_clojure/core_test.clj
+++ test/learning_clojure/core_test.clj
@@ -1,6 +1,6 @@
-(ns learning.core-test
+(ns learning-clojure.core-test
   (:require [clojure.test :refer :all]
-            [learning.core :refer :all]))
+            [learning-clojure.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
