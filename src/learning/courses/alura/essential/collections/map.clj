23aff071655ea520d6ff3bf575a25372de42ddad
Ygor Simões <contato.ygorsimoes@gmail.com>
2022-12-02 03:24:28 -0300

    update comments

--- src/learning/courses/alura/essential/collections/map.clj
+++ src/learning/courses/alura/essential/collections/map.clj
@@ -4,10 +4,10 @@
 (def prices [49 100 200 403 500 600 701])
 
 (defn added-one-if-pair
+  "Returns more one if even."
   [value]
   (if (even? value)
     (inc value)
     value))
 
-;; Return all values +1 (inc).
 (println "Map:" (map added-one-if-pair prices))
