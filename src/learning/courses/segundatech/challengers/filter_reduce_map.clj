72ae1159359002a11799a9cd7799c9cd5bae2a2e
Ygor Simões <contato.ygorsimoes@gmail.com>
2022-12-02 04:00:20 -0300

    refactoring and add final challenger

--- src/learning/courses/segundatech/challengers/filter_reduce_map.clj
+++ src/learning/courses/segundatech/challengers/filter_reduce_map.clj
@@ -9,35 +9,57 @@
   [year]
   (= year 5))
 
-(defn quantity-of-students-in-fifth-year
+(defn filter-quantity-of-students-in-fifth-year
   "Returns the quantity of the students in the fifth years"
   [students]
   (count (filter student-in-fifth-year? students)))
 
-(quantity-of-students-in-fifth-year [5 5 6 7 8 6 5 5])
-(quantity-of-students-in-fifth-year [5 6 5 5])
+(filter-quantity-of-students-in-fifth-year [5 5 6 7 8 6 5 5])
+(filter-quantity-of-students-in-fifth-year [5 6 5 5])
 
 
 ;; CHALENGER (reduce):
 ;; CHALLENGER one function that takes a vector of ages and return the sum.
 
-(defn summing-of-ages
+(defn reduce-summing-of-ages
   "Returns the sum of ages"
   [ages]
   (reduce + ages))
 
-(summing-of-ages [5 10 5])
+(reduce-summing-of-ages [5 10 5])
 
 
 ;; CHALLENGER (map)
 ;; Create one function that takes one vector of names and return the average
 ;; size of the names.
 
-(defn average-size-of-the-names
+(defn map-average-size-of-the-names
   "Returns the average size of the names"
   [names]
   (let [size-vector (count names)
-        size-of-the-names (reduce + (map count names))]
-    (/ size-of-the-names size-vector)))
+        sum-of-size-of-all-names (reduce + (map count names))]
+    (/ sum-of-size-of-all-names size-vector)))
 
-(average-size-of-the-names ["Marcio" "Gibson"])
+(map-average-size-of-the-names ["Marcio" "Gibson"])
+
+
+;; CHALLENGER (final)
+;; Create one function that take one vector of the name and return the average
+;; size of the names, but most ignore names with three or less caracteres.
+
+(defn less-or-equal-three?
+  "Return true if less or equal three else return false"
+  [number]
+  (> number 3))
+
+(defn final-average-size-of-the-names
+  "Returns the average size of the names, but most ignore
+  names with three or less caracteres"
+  [names]
+  (let [size-of-the-names (map count names)
+        size-of-the-names-without-three-char (filter less-or-equal-three? size-of-the-names)
+        size-vector-without-three-char (count size-of-the-names-without-three-char)
+        sum-of-size-of-all-names (reduce + size-of-the-names-without-three-char)]
+    (/ sum-of-size-of-all-names size-vector-without-three-char)))
+
+(final-average-size-of-the-names ["Ana" "Marcio" "Gibson"])
