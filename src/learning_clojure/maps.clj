0a0960b4256d1b85bf1ceebf1999a26074847515
Ygor Simões <ygorsimoes@hotmail.com>
2023-07-28 22:47:53 -0300

    feat: add content maps

--- src/learning_clojure/maps.clj
+++ src/learning_clojure/maps.clj
@@ -37,3 +37,51 @@
 
 ;; (into) is a function that adds a collection of key-value pairs to a map.
 (into inventory [[:potion 2] [:gold 100]])
+
+(def inventory-complexity {:potion {:quantity 5
+                                    :price    10}
+                           :sword  {:quantity 1
+                                    :price    200}
+                           :shield {:quantity 1
+                                    :price    50}})
+
+;; (get-in) is a function that returns a value from a nested map.
+(get-in inventory-complexity [:potion :quantity])
+
+;; (assoc-in) is a function that adds a key-value pair to a nested map.
+(assoc-in inventory-complexity [:potion :quantity] 10)
+
+;; (update-in) is a function that updates a value in a nested map.
+(update-in inventory-complexity [:potion :quantity] inc)
+
+;; (select-keys) is a function that returns a map with only the specified keys.
+(select-keys inventory-complexity [:potion :sword])
+
+;; (zipmap) is a function that creates a map from two collections.
+(zipmap [:potion :sword] [{:quantity 5 :price 10}
+                          {:quantity 1 :price 200}])
+
+;; Threads First:
+(-> inventory-complexity
+    :potion
+    :quantity
+    println)
+
+(-> inventory-complexity
+    (assoc-in [:potion :quantity] 11)
+    (update-in [:potion :quantity] inc)
+    (get-in [:potion :quantity]))
+
+
+;; Threads Last:
+(->> inventory-complexity
+    :potion
+    :quantity
+    println)
+
+(->> inventory-complexity
+     :potion
+     vals
+     (map inc)
+     vec
+     println)
